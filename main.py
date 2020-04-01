40fc207d28786b6fab4315981cdccac035c4936c
arry <1915791114@qq.com>
2020-04-01 22:58:48 +0800

    1.0

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from flask import Flask
 from flask import request
+import user as db
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "helloworld"
 
+   return str(db.select_user())
 
-@app.route("/add")
+@app.route("/delete")
+def deleteUser():
+  name= request.args.get("name")
+  db.delect_user(name)
+  return '删除成功'
+# @app.route("/add")
+# def add():
+#     a = request.args.get("a")
+#     b = request.args.get("b")
+#     return a + b
+
+@app.route("/addUser")
 def add():
-    a = request.args.get("a")
-    b = request.args.get("b")
-    return a + b
+    name = request.args.get("name")
+    mail = request.args.get("mail")
+    age = request.args.get("age")
+    db.add_user(name, mail, age)
+    return '添加成功'
+
+
 
 
 # http://127.0.0.1:5000/add?a=1&b=2
