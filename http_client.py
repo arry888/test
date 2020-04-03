aa8169f4153b64f9da3c86623f0f277ad9282989
李大志 <lidazhi@58.com>
2020-04-03 16:19:36 +0800

    2020/4/3

---
+++ http_client.py
@@ -0,0 +1,33 @@
+import requests
+
+r = requests.get('https://www.baidu.com')
+print(r.status_code)
+# print(r.headers)
+# for k in r.headers:
+#     print(r.headers[k])
+# print(r.headers['content-type'])
+# print(r.text)
+
+
+# r1 = requests.get('https://api.github.com/events', auth=('user', 'pass'))
+# print(r1.text)
+# print(r1.status_code)
+
+
+r2 = requests.post('http://httpbin.org/post', data={'key': 'value'})
+# print(r2)
+# print(r2.text)
+# print(r2.json())
+# print(r2.json()['form'])
+
+
+payload = {'key1': 'value1', 'key2': 'value2'}
+headers = {'user-agent': 'my-app/0.0.1'}
+r = requests.get("http://httpbin.org/get", params=payload)
+print(r.status_code)
+print(r.headers)
+print(r.url)
+print(r.encoding)
+
+r = requests.post("http://httpbin.org/post", data=payload)
+print(r.text)
